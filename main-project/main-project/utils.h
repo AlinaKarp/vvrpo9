143b8c288316c88e0eb642adc6af8bdb490bc2f1
AlinaKarp <karpala628@gmail.com>
2024-05-30 17:33:17 +0300

    utils.h

--- main-project/main-project/utils.h
+++ main-project/main-project/utils.h
@@ -1,1 +1,18 @@
 #pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <vector>
+
+struct InternetUsage {
+    std::string start_time;
+    std::string end_time;
+    int received_bytes;
+    int sent_bytes;
+    std::string program;
+};
+
+void filterAndSortData(const std::string& filename);
+
+#endif // UTILS_H
