c7a072f4d3755fedd99b8d300c57aa5b9c0a5fc8
AlinaKarp <karpala628@gmail.com>
2024-05-30 17:10:43 +0300

    создана заготовка

---
+++ main-project/main-project/main-project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #5. Протокол работы в Интернет\n";
+    cout << "Author: Алина Карпусенка \n";
+    return 0;
+}
